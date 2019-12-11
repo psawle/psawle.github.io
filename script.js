fea5472420ae4bf4d55c74fc6591844882130ae3
psawle <psawle4@gmail.com>
2019-12-11 17:02:23 +0530

    changed script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+ $(".port-items").click(function() {
+        $(".collapse").collapse("hide");
+      });
+
+      $(document).on("click", '[data-toggle="lightbox"]', function(e) {
+        e.preventDefault();
+        $(this).ekkolightbox();
+      });
